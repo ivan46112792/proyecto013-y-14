f40bfc02c0156f2243c77da73720d46e0c4715ae
ivan46112792 <108637538+ivan46112792@users.noreply.github.com>
2023-04-13 14:22:39 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import { useState } from 'react';
 
 function App() {
-  const [numero, setNumero] = useState('')
+  const [texto, setTexto] = useState('')
 
-  function cambioNumero(event) {
-    const entrada = event.target.value;
-    let cant = 0;
-    for (let x = 0; x < entrada.length; x++)
-      if (entrada[x] === '0' || entrada[x] === '1')
-        cant++;
-    if (cant === entrada.length)
-      setNumero(entrada)
+  function cambioTexto(e) {
+    setTexto(e.target.value)
   }
 
   return (
     <div>
-      <p>Ingrese un número binario:
-        <input type="text" value={numero} onChange={cambioNumero} />
-      </p>
+      <p><textarea value={texto} onChange={cambioTexto} cols="100" rows="5"></textarea></p>
+      <p>Cantidad de caracteres ingresados: {texto.length}</p>
     </div>
   );
 }
